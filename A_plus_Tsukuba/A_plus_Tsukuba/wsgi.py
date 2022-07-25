81df2f7c127aec281608438fd034936597056dd4
Takumi Tamura <tamutamuphone@gmail.com>
2022-07-25 16:09:33 +0900

    djangoプロジェクト作成
    ```django-admin startproject A_plus_Tsukuba```を実行しただけ

---
+++ A_plus_Tsukuba/A_plus_Tsukuba/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for A_plus_Tsukuba project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A_plus_Tsukuba.settings')
+
+application = get_wsgi_application()
