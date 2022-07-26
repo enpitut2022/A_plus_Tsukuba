831840677e91c825f6b6eecd634ba0452658bc67
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-07-26 11:53:23 +0900

    #PBI2 task726-5 モデルを定義した。

--- board/models.py
+++ board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+class Post(models.Model):
+
+    post_id = models.UUIDField(verbose_name='投稿者id', primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField(verbose_name='日付', default=timezone.now)
+    sender_name = models.CharField(verbose_name='投稿者名(匿名)', max_length=40)
+    text = models.TimeField(verbose_name='本文')
+    created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
