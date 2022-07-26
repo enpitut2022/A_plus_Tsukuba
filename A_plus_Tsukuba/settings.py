cad2ff6484f58c6b8ac10bad2789898f752ab6fd
nitoca <78777225+nitoca@users.noreply.github.com>
2022-07-26 14:53:54 +0900

    726-2-first

--- A_plus_Tsukuba/settings.py
+++ A_plus_Tsukuba/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# added
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- board/views.py
+++ board/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world.\nチーム名: 半分、青い。\nプロダクト名: A+つくば")
+    # return HttpResponse("index")
+    # return render(request, 'board/index.html')
+    return render( request, 'board/Chat.html' )
+    # return HttpResponse("Hello, world.\nチーム名: 半分、青い。\nプロダクト名: A+つくば")
+    # # change to /Chat/
+    # return render(request, 'board/Chat.html')
+
+# def Chat(request):
+#     return render(request, 'board/Chat.html')
+#     # return HttpResponse("chat")
