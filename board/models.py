f45cda5d9c4936678abdc8e9fdf29744fcc4c91f
Takumi TAMURA <tamutamuphone@gmail.com>
2022-07-26 12:07:05 +0900

    本文をTextFieldに。日付はいらないので削除。

--- board/models.py
+++ board/models.py
@@ -5,7 +5,6 @@ import uuid
 class Post(models.Model):
 
     post_id = models.UUIDField(verbose_name='投稿者id', primary_key=True, default=uuid.uuid4, editable=False)
-    date = models.DateField(verbose_name='日付', default=timezone.now)
     sender_name = models.CharField(verbose_name='投稿者名(匿名)', max_length=40)
-    text = models.TimeField(verbose_name='本文')
+    text = models.TextField(verbose_name='本文')
     created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
