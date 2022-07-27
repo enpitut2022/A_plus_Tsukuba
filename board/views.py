6d868c717db486dcbafa5700ddc26147e3a1b2a2
Takumi TAMURA <tamutamuphone@gmail.com>
2022-07-27 10:10:16 +0900

    書き込み機能の実装

--- board/views.py
+++ board/views.py
@@ -8,3 +8,9 @@ class Index(ListView):
     model = Post
     context_object_name = 'post_data'
     ordering = ['-created_at']
+
+    def post(self, request, *args, **kwargs):
+        name = self.request.POST.get("username", None)
+        msg = self.request.POST.get("message", None)
+        self.model.objects.create(sender_name=name, text=msg)
+        return self.get(request, *args, **kwargs)
