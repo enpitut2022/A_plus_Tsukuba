aa14e2a1c3af59b963d954a251ce0e72b8edeeb9
Takumi Tamura <tamutamuphone@gmail.com>
2022-07-28 15:55:17 +0900

    a Co-authored-by: A.M.R <ssf2xguile@users.noreply.github.com>

--- board/views.py
+++ board/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
@@ -7,6 +8,9 @@ from django.views.generic import TemplateView, ListView
 class Index(ListView):
     template_name = "board/Chat.html"
     model = Post
+    em_count = model.objects.filter(emotion=0).count()
+    print(em_count)
+    context = {"em_count" : em_count}
     context_object_name = 'post_data'
     ordering = ['-created_at']
 
@@ -16,3 +20,6 @@ class Index(ListView):
         emo = self.request.POST.get("emotion_type", "0")
         self.model.objects.create(sender_name=name, text=msg, emotion=emo)
         return redirect("index")
+
+    def get(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
