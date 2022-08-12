ee94f33f26f52b4bb8166300f27669c16bc8de34
Takumi Tamura <tamutamuphone@gmail.com>
2022-08-13 03:56:46 +0900

    雑１

--- board/urls.py
+++ board/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
+from . import apis
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('threads/<int:thread_id>/', views.ThreadView.as_view(), name='threads'),
     path('create_threads/', views.CreateThreadView.as_view(), name='create_threads'),
-    # path('Chat/', views.Chat, name='Chat'),
+    path('api/', apis.api.as_view(), name = "api"),
 ]
