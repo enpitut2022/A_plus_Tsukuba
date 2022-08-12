c47b0fd808b11f7fd37ee53cc065377d233f6024
Takumi Tamura <tamutamuphone@gmail.com>
2022-08-13 04:12:59 +0900

    雑２

--- board/static/board/chat.js
+++ board/static/board/chat.js
@@ -1,4 +1,4 @@
-let vue_msg = new Vue({
+const vue_msg = new Vue({
     delimiters: ['[[', ']]'],   //必ず必要
     el: '#vue_msg',
     data: {
@@ -6,3 +6,15 @@ let vue_msg = new Vue({
     }
 });
 
+const test_api = new Vue({
+    el: "#api_test",
+    data: {
+        message: null
+    },
+    delimiters: ['[[', ']]'],
+    mounted () {
+        axios
+          .get('/api')
+          .then(response => (this.message = response))
+      }
+})
