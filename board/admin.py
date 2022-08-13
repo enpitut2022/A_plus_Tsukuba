ae8b9444ad5254fbdc542a8f0836b3d9545e3494
ssf2xguile <iwanatteiwanai123@gmail.com>
2022-08-13 14:57:02 +0900

    モデルを再定義してフロント側にスレッド情報を取得できるようにした。 古いコードを消した。
    Co-authored-by: 田村匠 <s2213512@u.tsukuba.ac.jp>

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Thread
+from .models import Post, Thread,Reply
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Thread)
+admin.site.register(Reply)
--- board/urls.py
+++ board/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('threads/<int:thread_id>/', views.ThreadView.as_view(), name='threads'),
-    path('create_threads/', views.CreateThreadView.as_view(), name='create_threads'),
-    # path('Chat/', views.Chat, name='Chat'),
 ]
