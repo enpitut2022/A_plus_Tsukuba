da81c8b71a8a6af409e3f222ac688042f1d87990
Takumi Tamura <tamutamuphone@gmail.com>
2022-08-13 17:33:46 +0900

    APIの実装
    Co-authored-by: A.M.R <ssf2xguile@users.noreply.github.com>

--- board/serializers.py
+++ board/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
 from .models import Post, Reply
 
-class PostSerializer(serializers.ModelSerializer):
+class GetPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post                   
         fields = ["post_id","sender_name","text","created_at","emotion"]  
 
-
-class ReplySerializer(serializers.ModelSerializer):
+class GetReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply                   
         fields = ["reply_id","sender_name","text","created_at","emotion"]  
+
+class PostPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post                   
+        fields = ["post_id","sender_name","text","created_at","emotion", "thread"]  
+        read_only_fields = ('post_id',"created_at")
+
+class PostReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply                   
+        fields = ["reply_id","sender_name","text","created_at","emotion","post_id"]
+        read_only_fields = ('reply_id',"created_at") 
