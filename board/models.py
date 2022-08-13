bc6ea274ba9641c7ac3e7a59c6a3bf426e8ea7d4
Takumi Tamura <tamutamuphone@gmail.com>
2022-08-14 02:53:09 +0900

    スレッド作成プログラム

--- board/models.py
+++ board/models.py
@@ -1,12 +1,26 @@
+from concurrent.futures import thread
+from turtle import title
 from django.db import models
 from django.utils import timezone
 import uuid
 
 class Thread(models.Model):
     title    = models.CharField(verbose_name='スレタイ',blank=False, null=False, max_length=150)
-    created_at = models.DateTimeField(verbose_name='作成日時', default=timezone.now)
     def __str__(self):
         return self.title
+
+class Subject(models.Model):
+    internal_id = models.IntegerField(verbose_name="内部id", primary_key=True, default=0)
+    code = models.CharField(verbose_name="科目番号", max_length=10, default="")
+    name = models.CharField(verbose_name='講義名',blank=False, null=False, max_length=150)
+    teachers =  models.TextField(verbose_name='教員名', blank=True, max_length=100, default="")
+    subtype =  models.TextField(verbose_name='種類', blank=True, max_length=32, default="")
+    schools = models.CharField(verbose_name="学群等", max_length=4, default="", blank=True)
+    colleges = models.CharField(verbose_name="学類等", max_length=40, default="", blank=True)
+    thread_id = models.ForeignKey(Thread, verbose_name="スレッドid", on_delete=models.CASCADE, default=0)
+
+    def __str__(self):
+        return self.code + " " + self.name
 
 class Post(models.Model):
     EMOTION = (
