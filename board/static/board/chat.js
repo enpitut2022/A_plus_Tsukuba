9d08d55f14f5259cb25e918effcc1fe462601c8c
monokemonoke <kawabataryuhei@gmail.com>
2022-08-13 18:09:45 +0900

    子スレッドを表示できるようにした

--- board/static/board/chat.js
+++ board/static/board/chat.js
@@ -10,15 +10,35 @@ const test_api = new Vue({
     el: "#api_test",
     data: {
         message: null,
-        thread_id: document.getElementById('thread_num').value
+        child_message: null,
+        thread_id: document.getElementById('thread_num').value,
+        post_id: null
     },
     delimiters: ['[[', ']]'],
+    methods: {
+        // 親スレを取得する関数
+        async fetch_subthreads() {
+            let res = await axios.get(`/api/get_subthreads?thread_id=${this.thread_id}`)
+            console.table(res.data);
+            // 非同期関数
+            this.message = res;
+        },
+        // 子スレを取得する関数
+        async child_threads(post_id) {
+            this.child_message = [];
+            let child_res = await axios.get(`/api/get_replies?post_id=${post_id}`)
+            console.log('called child_threads');
+            console.table(child_res.data);
+
+            this.child_message = child_res.data || [];
+        }
+    },
     mounted () {
-        axios
-          .get(`/api/get_subthreads?thread_id=${this.thread_id}`)
-          .then(response => (this.message = response))
-      }
+        this.fetch_subthreads();
+    }
 })
+
+// http://127.0.0.1:8000/api/get_replies?post_id=4bff7065-6a7e-4dca-a18d-2d8fb430ac61
 
 // const less = `<div class="row">
 // <div class="col">
