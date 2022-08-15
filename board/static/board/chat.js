2a4161e8c8881a816f9f7ff1609e743190c27071
monokemonoke <kawabataryuhei@gmail.com>
2022-08-15 14:20:40 +0900

    wip

--- board/static/board/chat.js
+++ board/static/board/chat.js
@@ -1,54 +1,73 @@
-const vue_msg = new Vue({
-    delimiters: ['[[', ']]'],   //必ず必要
-    el: '#vue_msg',
-    data: {
-        message: 'Hello Vue!'
-    }
-});
-
 const test_api = new Vue({
-    el: "#api_test",
+    el: '#post_content',
     data: {
         message: null,
         child_message: {},
         thread_id: document.getElementById('thread_num').value,
-        post_id: null
+        post_id: null,
     },
     delimiters: ['[[', ']]'],
     methods: {
         // 親スレを取得する関数
         async fetch_subthreads() {
-            let res = await axios.get(`/api/get_subthreads?thread_id=${this.thread_id}`)
+            let res = await axios.get(
+                `/api/get_subthreads?thread_id=${this.thread_id}`
+            );
             console.table(res.data);
             this.message = res;
         },
         // 子スレを取得する関数
         async child_threads(post_id) {
             this.$set(this.child_message, post_id, []);
-            let child_res = await axios.get(`/api/get_replies?post_id=${post_id}`)
+            let child_res = await axios.get(`/api/get_replies?post_id=${post_id}`);
             console.log('called child_threads');
             console.table(child_res.data);
 
             this.$set(this.child_message, post_id, child_res.data || []);
-        }
+        },
+
+        formatTimeString(time) {
+            let ts = Date.parse(time);
+            let date = new Date(ts);
+
+            let month = date.getMonth();
+            let year = date.getFullYear();
+            let day = date.getDay();
+            let hour = date.getHours().toString().padStart(2, '0');
+            let min = date.getMinutes().toString().padStart(2, '0');
+
+            return `${year}/${month}/${day} ${hour}:${min}`;
+        },
+
+        getCSS(id) {
+            const dict = {
+                0: 'badge badge-pill badge-danger',
+                1: 'badge badge-pill badge-warning',
+                2: 'badge badge-pill badge-success',
+                3: 'badge badge-pill badge-primary',
+                4: `badge badge-pill badge-info`,
+                5: 'badge badge-pill badge-Light',
+            };
+
+            return dict[id];
+
+        },
+
+        getText(id) {
+            const dict = {
+                0: '非常事態(´•_•; )',
+                1: 'じっくり(-ω-;)ｳｰﾝ',
+                2: '助かった(*´▽`人)',
+                3: '提案(^^)/~~~',
+                4: `よしよし('ω')`,
+                5: '大丈夫？( *´艸｀)',
+            };
+
+            return dict[id];
+
+        },
     },
-    mounted () {
+    mounted() {
         this.fetch_subthreads();
-    }
-})
-
-// http://127.0.0.1:8000/api/get_replies?post_id=4bff7065-6a7e-4dca-a18d-2d8fb430ac61
-
-// const less = `<div class="row">
-// <div class="col">
-//   <div id="vue_msg" class="lead">
-//     [[ message ]]
-//   </div>
-//   <div id="api_test" class="content">
-//     <div v-for="msg in message.data" :key="msg.post_id">
-//       <div>[[ msg.sender_name ]]</div>
-//       <div>[[ msg.text ]]</div>
-//     </div>
-//   </div>
-// </div>
-// </div>`
+    },
+});
