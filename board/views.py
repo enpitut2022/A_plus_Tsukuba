5b6f4dada4316ccc7867258a661a012949296e24
Takumi Tamura <tamutamuphone@gmail.com>
2022-08-23 19:36:24 +0900

    非常事態宣言ランキング

--- board/views.py
+++ board/views.py
@@ -1,9 +1,11 @@
+from concurrent.futures import thread
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post, Subject, Thread
+from .models import Post, Reply, Subject, Thread
 from django.shortcuts import redirect
 from django.views.generic import TemplateView, ListView
+from django.db.models import Count
 
 class Index(ListView):
     def get(self, request, *args, **kwargs):
@@ -36,4 +38,29 @@ class SearchView(ListView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['post_list'] = self.model.objects.all().order_by('-created_at')[:10]
+
+        # emergency ranking
+        thread_dict = dict() # thread_dict[thread_id] = 非常事態数
+        # post
+        target_data = Post.objects.filter(emotion=0)
+        groupby_data = target_data.values("thread_id").annotate(total=Count("thread_id"))
+        for col in groupby_data:
+            thread_dict[col["thread_id"]] = col["total"]
+        # reply
+        target_data = Reply.objects.filter(emotion=0)
+        groupby_data = target_data.values("post_id").annotate(total=Count("post_id"))
+        for col in groupby_data:
+            thread_id = Post.objects.filter(post_id=col["post_id"]).values("thread")[0]["thread"]
+            thread_dict[thread_id] += col["total"]
+        
+        ranking = []
+        ranking_nums = sorted(thread_dict.items(), key=lambda x:x[1], reverse=True)[:5]
+        for tid , num  in ranking_nums:
+            title = Thread.objects.filter(pk=tid).values("title")[0]["title"]
+            ranking.append(
+                (tid, title, num)
+            )
+
+        context["ranking"] = ranking
+
         return context
