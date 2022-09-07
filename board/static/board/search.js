2af0c6942c9bc041758d55e58df0b233bf34966c
Takumi Tamura <tamutamuphone@gmail.com>
2022-09-07 09:13:42 +0900

    変数名を再考 #19

--- board/static/board/search.js
+++ board/static/board/search.js
@@ -4,42 +4,42 @@ Vue.createApp({
         return {
             results: {},
             query: "",
-            request_time : Date.now()
+            latest_request_time : Date.now()
         };
     },
     methods: {
         inputEvent(event) {
             //検索ボックスの内容が変更されるたびに呼ばれる
-            const value = event.target.value.trim();
-            this.searchAndDataUpdate(value);
+            const input_value = event.target.value.trim();
+            this.searchAndDataUpdate(input_value);
         },
-        async searchAndDataUpdate(query) {
-            if (!query) {
+        async searchAndDataUpdate(input_value) {
+            if (!input_value) {
                 this.results = {};
                 this.query = "";
             }else{    
                 //APIリクエストは非同期のため、重いリクエストの後に軽いリクエストを送ると
                 //軽い＝＞重いの順に到着してしまい、結果がおかしくなります。
                 //そのため、時刻を使って最新のリクエストのみデータを更新するようにしています。
-                const called_now = Date.now();
-                this.request_time = called_now;
+                const called_time = Date.now();
+                this.latest_request_time = called_time;
 
                 const ret = await axios.get(
-                    `/api/search_subjects?q=${query}`
+                    `/api/search_subjects?q=${input_value}`
                 );
                 
-                if (this.request_time == called_now) {
+                if (this.latest_request_time == called_time) {
                     this.results = ret.data;
-                    this.query = query;
+                    this.query = input_value;
                 }                
             }
         },
-        readmoreFilter(text, length, suffix) {
+        readmoreFilter(text, max_length, suffix) {
             //長いテキストを省略表記にする
             //readmoreFilter("ABC", 4, "...") => "ABC"
             //readmoreFilter("ABCD", 2, "...") => "AB..."
-            if (text.length < length) return text;
-            return text.substring(0, length) + suffix;
+            if (text.length < max_length) return text;
+            return text.substring(0, max_length) + suffix;
         }
     }
 }).mount('#search_app')
