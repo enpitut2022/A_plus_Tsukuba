52f472ce13dda869f177de8736d0db0c26bf7180
Takumi Tamura <tamutamuphone@gmail.com>
2022-09-07 09:05:42 +0900

    searchの書き直し #19

--- board/static/board/search.js
+++ board/static/board/search.js
@@ -1,48 +1,45 @@
-Vue.filter('readmore', function(text, length, suffix){
-    if (text.length < length) return text;
-    return text.substring(0, length) + suffix
-})
-
-Vue.component('child-component', {
-    template: '<input id="search_input" v-model.trim="query" @input="onInput" type="text" class="form-control" placeholder="科目名を入力してください。">',
-    methods: {
-        onInput: function() {
-            this.$emit('childevent', this.query);
-        }
-    }
-});
-
-new Vue({
-    el: '#search_app',
-    delimiters: ['[[', ']]'],   //必ず必要
-    data: {
-        results: {},
-        query: "",
-        request_time : Date.now()
+Vue.createApp({
+    delimiters: ['[[', ']]'],
+    data() {
+        return {
+            results: {},
+            query: "",
+            request_time : Date.now()
+        };
     },
     methods: {
-        async searchMethod (query="") {
-            /*
-                APIリクエストは非同期のため、
-                重いリクエストの後に軽いリクエストを送ると
-                軽い＝＞重いの順に到着してしまい、
-                結果がおかしくなります。
-                そのため、時刻を使って最新のリクエストのみ表示するようにしてます。    
-            */
-            const now = Date.now();
-            this.request_time = now;
-            if (query == "") {
+        inputEvent(event) {
+            //検索ボックスの内容が変更されるたびに呼ばれる
+            const value = event.target.value.trim();
+            this.searchAndDataUpdate(value);
+        },
+        async searchAndDataUpdate(query) {
+            if (!query) {
                 this.results = {};
-                this.query = query;
-                return;
-            }
-            const res = await axios.get(
-                `/api/search_subjects?q=${query}`
-            );
-            if (this.request_time == now) {
-                this.results = res.data;
-                this.query = query;
+                this.query = "";
+            }else{    
+                //APIリクエストは非同期のため、重いリクエストの後に軽いリクエストを送ると
+                //軽い＝＞重いの順に到着してしまい、結果がおかしくなります。
+                //そのため、時刻を使って最新のリクエストのみデータを更新するようにしています。
+                const called_now = Date.now();
+                this.request_time = called_now;
+
+                const ret = await axios.get(
+                    `/api/search_subjects?q=${query}`
+                );
+                
+                if (this.request_time == called_now) {
+                    this.results = ret.data;
+                    this.query = query;
+                }                
             }
         },
+        readmoreFilter(text, length, suffix) {
+            //長いテキストを省略表記にする
+            //readmoreFilter("ABC", 4, "...") => "ABC"
+            //readmoreFilter("ABCD", 2, "...") => "AB..."
+            if (text.length < length) return text;
+            return text.substring(0, length) + suffix;
+        }
     }
-})
+}).mount('#search_app')
