25db3c37dda5b9383ec81bb6989e329bd62e70e8
nitoca <78777225+nitoca@users.noreply.github.com>
2022-09-14 14:46:48 +0900

    document.cookieを使う（ライブラリを使わない方法）ならできた

--- board/static/board/bookmark.js
+++ board/static/board/bookmark.js
@@ -1,65 +1,130 @@
+// Vue.createApp({
+//     delimiters: ['[[', ']]'],
+//     data() {
+//         return {
+//             agree: true
+//         };
+//     }
+// }).mount('#app');
 
-const bookmark_toggle = new Vue({
-        el: '#bookmark_toggle',
-        data: {
-                checked: false,
-                thread_id: document.getElementById('thread_num').value,
-                thread_title: document.getElementById('thread_title').value,
-        },
-        delimiters: ['[[', ']]'],
-        methods: {
-            loadbookmark() {
-                const bookmark_key = "bookmark";
-                const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す                
-                let bookmark_json = {};
-                if(bookmark_value){
-                        bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
-                }
-                if(this.thread_id in bookmark_json){
-                    this.checked = true;
-                }
-            },
-            judge_checked(){              
-                if(this.checked == true){
-                    setBookmark(this.thread_id, this.thread_title);
-                }
-                else{
-                    deleteBookmark(this.thread_id);
-                }
+// const bookmark_toggle = new Vue({
+//         el: '#bookmark_toggle',
+//         data: {
+//                 checked: false,
+//                 thread_id: document.getElementById('thread_num').value,
+//                 thread_title: document.getElementById('thread_title').value,
+//         },
+//         delimiters: ['[[', ']]'],
+//         methods: {
+//             loadbookmark() {
+//                 const bookmark_key = "bookmark";
+//                 const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す                
+//                 let bookmark_json = {};
+//                 if(bookmark_value){
+//                         bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
+//                 }
+//                 if(this.thread_id in bookmark_json){
+//                     this.checked = true;
+//                 }
+//             },
+//             judge_checked(){              
+//                 if(this.checked == true){
+//                     setBookmark(this.thread_id, this.thread_title);
+//                 }
+//                 else{
+//                     deleteBookmark(this.thread_id);
+//                 }
+//             }
+//         },
+//         mounted() {        
+//             this.loadbookmark();
+//         },
+// });
+
+
+
+// change to vue3
+
+const BOOK_MARK = {}
+BOOK_MARK.EXPIRES = 365 * 6; //6 years
+
+BOOK_MARK.setCookie = function(name, json){
+    Cookies.set(name, json, { expires: BOOK_MARK.EXPIRES, path: '' });
+};
+
+BOOK_MARK.setBookmark = function(thread_id, thread_title) {
+    // // load cookie
+    const bookmark_key = "bookmark";
+    const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す        
+    let bookmark_json = {};
+    if(bookmark_value){
+            bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
+    }
+    // add bookmark
+    bookmark_json[thread_id] = encodeURI(thread_title);
+    bookmark_json[thread_id] = thread_title;
+    console.log(bookmark_json);
+    // BOOK_MARK.setCookie(bookmark_key, bookmark_json); 
+    setCookie(bookmark_key, bookmark_json);
+
+}
+// {10: 'ファーストイヤーセミナー(秋山 肇)'}
+// { foo: "bar" }
+
+
+BOOK_MARK.deleteBookmark = function(thread_id){
+    // delete bookmark
+    const bookmark_key = "bookmark";
+    const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す
+
+    let bookmark_json = {};
+    if(bookmark_value){
+            bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
+    }
+    delete bookmark_json[thread_id]
+    // BOOK_MARK.setCookie(bookmark_key, bookmark_json);
+    setCookie(bookmark_key, bookmark_json);
+}
+
+
+
+Vue.createApp({
+    delimiters: ['[[', ']]'],
+    data() {
+        return {
+            checked: false,
+            thread_id: document.getElementById('thread_id').value,
+            thread_title: document.getElementById('thread_title').value,
+        }
+    },
+    methods: {
+        loadbookmark() {
+            const bookmark_key = "bookmark";
+            const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す                
+            let bookmark_json = {};
+            if(bookmark_value){
+                    bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
+            }
+            if(this.thread_id in bookmark_json){
+                this.checked = true;
             }
         },
-        mounted() {        
-            this.loadbookmark();
-        },
-});
-
-
-
-function setBookmark(thread_id, thread_title) {
-        // load cookie
-        const bookmark_key = "bookmark";
-        const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す        
-        let bookmark_json = {};
-        if(bookmark_value){
-                bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
+        judge_checked(){              
+            if(this.checked == true){
+                BOOK_MARK.setBookmark(this.thread_id, this.thread_title);
+            }
+            else{
+                BOOK_MARK.deleteBookmark(this.thread_id);
+            }
         }
-        // add bookmark
-        bookmark_json[thread_id] = encodeURI(thread_title);
-        setCookie(bookmark_key, bookmark_json);
-}
+    },
+    mounted() {
+        this.loadbookmark();
+    }
 
-function deleteBookmark(thread_id){
-        // delete bookmark
-        const bookmark_key = "bookmark";
-        const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す
+}).mount('#bookmark_toggle');
 
-        let bookmark_json = {};
-        if(bookmark_value){
-                bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
-        }
-        delete bookmark_json[thread_id]
-        setCookie(bookmark_key, bookmark_json);        
-}
+
 
 const setCookie = (name, json)=>{
 
@@ -84,3 +149,30 @@ const setCookie = (name, json)=>{
     //Cookieを保存する
     document.cookie = cookies;
 };
+
+// function setBookmark(thread_id, thread_title) {
+//         // load cookie
+//         const bookmark_key = "bookmark";
+//         const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す        
+//         let bookmark_json = {};
+//         if(bookmark_value){
+//                 bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
+//         }
+//         // add bookmark
+//         bookmark_json[thread_id] = encodeURI(thread_title);
+//         setCookie(bookmark_key, bookmark_json);
+// }
+
+// function deleteBookmark(thread_id){
+//         // delete bookmark
+//         const bookmark_key = "bookmark";
+//         const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す
+
+//         let bookmark_json = {};
+//         if(bookmark_value){
+//                 bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
+//         }
+//         delete bookmark_json[thread_id]
+//         setCookie(bookmark_key, bookmark_json);        
+// }
+
--- board/static/board/common.js
+++ board/static/board/common.js
@@ -1,9 +1,29 @@
-const bookmark_dropdown = new Vue({
-    el: '#bookmark_dropdown',
-    data: {
-            bookmark_data: [],
-    },
+// const bookmark_dropdown = new Vue({
+//     el: '#bookmark_dropdown',
+//     data: {
+//             bookmark_data: [],
+//     },
+//     delimiters: ['[[', ']]'],
+//     methods: {
+//         loadbookmark() {
+//             const bookmark_key = "bookmark";
+//             const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す                
+//             let bookmark_json = {};
+//             if(bookmark_value){
+//                     bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
+//             }
+//             this.bookmark_data = Object.entries(bookmark_json);  
+//         },
+//     }
+// });
+
+Vue.createApp({
     delimiters: ['[[', ']]'],
+    data() {
+        return {
+            bookmark_data: [],
+        }
+    },
     methods: {
         loadbookmark() {
             const bookmark_key = "bookmark";
@@ -13,6 +33,6 @@ const bookmark_dropdown = new Vue({
                     bookmark_json = JSON.parse(bookmark_value.split('=')[1]); //bookmark_valueをJSONに変換して取得
             }
             this.bookmark_data = Object.entries(bookmark_json);  
-        },
+        }
     }
-});
+}).mount('#bookmark_dropdown');
