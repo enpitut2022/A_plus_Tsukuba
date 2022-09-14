976cbd9989d4bf13bb91e89abff375c5bd68d7c8
nitoca <78777225+nitoca@users.noreply.github.com>
2022-09-14 17:07:39 +0900

    refactoring

--- board/static/board/bookmark.js
+++ board/static/board/bookmark.js
@@ -1,54 +1,32 @@
 const BOOK_MARK = {}
-BOOK_MARK.EXPIRES = 2190; //6 years
+BOOK_MARK.EXPIRES = 365; //DAYS
 
-// BOOK_MARK.getCookies = function(){
-//     let load_json = Cookies.get('bookmark');
+BOOK_MARK.getCookies = function(){ // return (JSON)object not (JSON)string
+    let load_json_string = Cookies.get('bookmark');
 
-//     let bookmark_json = {};
-//     if(load_json){
-//             bookmark_json = JSON.parse(load_json); 
-//     }
-//     return bookmark_json;
-// }
+    let bookmark_json_object = {};
+    if(load_json_string){
+            bookmark_json_object = JSON.parse(load_json_string); 
+    }
+    return bookmark_json_object;
+}
 
-BOOK_MARK.setCookie = function(name, json){
-    console.log(json);
-    Cookies.set(name, json, { expires: BOOK_MARK.EXPIRES });
+BOOK_MARK.setCookie = function(name, json_object){ // receive (JSON)object not (JSON)string
+    Cookies.set(name, JSON.stringify(json_object), { expires: BOOK_MARK.EXPIRES });
 };
 
 BOOK_MARK.setBookmark = function(thread_id, thread_title) {
-    //load bookmark
-    const bookmark_key = "bookmark";
-    let load_json = Cookies.get('bookmark');
-    console.log(load_json);
+    let bookmark_json = BOOK_MARK.getCookies();
 
-    let bookmark_json = {};
-    if(load_json){
-            bookmark_json = JSON.parse(load_json); //bookmark_valueをJSONに変換して取得
-            console.log(bookmark_json);
-    }
-    // add bookmark
     bookmark_json[thread_id] = thread_title;
-    console.log(bookmark_json);
-    console.log(JSON.stringify(bookmark_json));
-    BOOK_MARK.setCookie(bookmark_key, JSON.stringify(bookmark_json)); 
-    // setCookie(bookmark_key, bookmark_json);
-
+    BOOK_MARK.setCookie("bookmark", bookmark_json);
 }
 
 BOOK_MARK.deleteBookmark = function(thread_id){
-    // delete bookmark
-    const bookmark_key = "bookmark";
-    let load_json = Cookies.get('bookmark');
-    console.log(load_json);
-    let bookmark_json = {};
-    if(load_json){
-            bookmark_json = JSON.parse(load_json); //bookmark_valueをJSONに変換して取得
-            console.log(bookmark_json);
-    }
+    let bookmark_json = BOOK_MARK.getCookies();
 
     delete bookmark_json[thread_id]
-    BOOK_MARK.setCookie(bookmark_key, JSON.stringify(bookmark_json));
+    BOOK_MARK.setCookie("bookmark", bookmark_json);
 }
 
 
@@ -57,30 +35,22 @@ Vue.createApp({
     delimiters: ['[[', ']]'],
     data() {
         return {
-            checked: false,
+            j_checked: false,
             thread_id: document.getElementById('thread_id').value,
             thread_title: document.getElementById('thread_title').value,
         }
     },
     methods: {
         loadbookmark() {
-            const bookmark_key = "bookmark";
-            let load_json = Cookies.get('bookmark');
-            console.log(load_json);
-            // const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す        
-            let bookmark_json = {};
-            if(load_json){
-                    bookmark_json = JSON.parse(load_json); //bookmark_valueをJSONに変換して取得
-                    console.log(bookmark_json);
-            }
-
+            let bookmark_json = BOOK_MARK.getCookies();
             
             if(this.thread_id in bookmark_json){
-                this.checked = true;
+                this.j_checked = true;
             }
         },
-        judge_checked(){              
-            if(this.checked == true){
+        judge_checked(){
+            this.j_checked = !this.j_checked;       
+            if(this.j_checked == true){
                 BOOK_MARK.setBookmark(this.thread_id, this.thread_title);
             }
             else{
--- board/static/board/common.js
+++ board/static/board/common.js
@@ -7,16 +7,7 @@ Vue.createApp({
     },
     methods: {
         loadbookmark() {
-
-            const bookmark_key = "bookmark";
-            let load_json = Cookies.get('bookmark');
-            console.log(load_json);
-            // const bookmark_value = document.cookie.split('; ').find(row => row.startsWith(bookmark_key)); //bookmarkが含まれる要素の配列を返す        
-            let bookmark_json = {};
-            if(load_json){
-                    bookmark_json = JSON.parse(load_json); //bookmark_valueをJSONに変換して取得
-                    console.log(bookmark_json);
-            }
+            let bookmark_json = BOOK_MARK.getCookies();
             this.bookmark_data = Object.entries(bookmark_json);  
         }
     }
